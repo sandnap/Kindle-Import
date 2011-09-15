69e3d33be76bccdc1dc08d34b0a88ba9a184be44
Kirt Henrie <kjhenrie@yahoo.com>
2011-09-14 23:19:15 -0600

    initial commit of filname import version

--- app/source/KindleImport.js
+++ app/source/KindleImport.js
@@ -3,6 +3,7 @@ enyo.kind ({
 	kind: enyo.VFlexBox,
 	books: [],
 	bookAsinsInDB: "",
+	importCount: 0,
 	components: [
 		{kind: "AppMenu", name: "appMenu", lazy: false, components: [
 			{caption: $L("About"), onclick: "showAbout"},
@@ -26,73 +27,61 @@ enyo.kind ({
 					"<br/>&nbsp;&nbsp;}" +
 					"<br/>&nbsp;}" +
 					"<br/>]"},
-				{content: "<br/>2. Create a text file (e.g. book-import.txt) with a list of the books you wish to import (one per line) in the format:"},
-				{content: "<br/>&nbsp;&nbsp;<i>asin|title|author|file</i> (e.g. GGGGGGG|My Book|Doe, John|My Book - Doe, John.mobi)*."},
-				{content: "<br/>3. Copy the text file to your Touchpad (root of internal is fine)"},
-				{content: "4. Create the folder /media/internal/.kindle-import on your touchpad and copy the books you wish to import into it"},
-				{content: "5. Click on the button below, select the import text file and follow the prompts<br/><br/>"},
-				{kind: enyo.Button, name: "fileSelectButton", content: "Select metadata File and Import", onclick: "openFilePicker"},
+				{content: "2. Create the folder /media/internal/.kindle-import on your touchpad and copy the books you wish to import into it"},
+				{content: "3. Click on the button below to import the books not already in the database<br/><br/>"},
+				{kind: enyo.Button, name: "fileSelectButton", content: "Import Books", onclick: "loadBooks"},
 				{content: "You can use the Delete button below to remove all books imported by this utility from Kindle.  This does not delete the book files."},
 				{kind: enyo.Button, name: "deleteBooksButton", content: "Delete All Imported Books From Kindle", onclick: "delBooks"},
 				{content: "<br/>* The asin must be unique for each book. If you wish to follow the Kindle book naming conventions you will need to set the asin field to the field published on the" +
 				" Amazon page for the book and the filename should be &lt;asin&gt;_EBOK.mobi."},
-				{content: "Roadmap: Add support for cover images. Read book metadata from the file and eliminate the import file. Do something about the hardcoded total locations value (currently 15000 to support large books)."},
+				{content: "Roadmap: Add support for cover images. Add fields to edit the book metadata, Do something about the hardcoded total locations value (currently 15000 to support large books)."},
 				{content: "<br/><b>WARNING:  Use this application at your own risk. The author of this application is not responsible for damage caused or data lost while using this application.</b>"},
 			]}
 		]},
 		{kind: "KindleImportService", name: "kindleImport"},
-		{kind: "FilePicker", name: "filePicker", onPickFile: "metaFilePicked"},
-		{kind: "WebService", name: "metaFileService", onSuccess: "parseMetaFile", handleAs: "text"},
 		{kind: "Popup", name:"dialog"},
-		{name:"addBooksEntry", kind: "DbService", dbKind: "com.palm.kindle.books:1", method: "put", onFailure: "dbFailure", onSuccess: "importSuccess"},
+		{name:"addBooksEntry", kind: "DbService", dbKind: "com.palm.kindle.books:1", method: "put", onFailure: "dbFailure"},
 		{name:"delBooksEntry", kind: "DbService", dbKind: "com.palm.kindle.books:1", method: "del", onFailure: "dbFailure", onSuccess: "deleteSuccess"},
 		{name:"findBooksEntry", kind: "DbService", dbKind: "com.palm.kindle.books:1", method: "find", onFailure: "dbFailure", onSuccess: "findResult"}
 	],
-	openFilePicker: function(inSender, inResponse) {
-		//this.loadBooks();
-		this.booksInDB();
-		this.$.filePicker.pickFile();
-	},
-	metaFilePicked: function(inSender, inResponse) {
-		var fileName = enyo.json.stringify(inResponse);
-		if (typeof fileName.split(":")[1] == 'undefined') {
-			this.$.filePicker.close();
-			return;
-		}
-		var path = fileName.split(":")[1].split("\"")[1];
-		this.$.metaFileService.setUrl("file://" + path);
-		this.$.metaFileService.call();
-	},
-	parseMetaFile: function(inSender, inResponse) {
-		var bks = inResponse.split("\n");
-		for (var i = 0; i < bks.length; i++) {
-			var b = bks[i];
-			if (b.length > 0) {
-				var book = new Book().setup(b);
-				if (this.bookAsinsInDB.indexOf('"' + book.asin + '"') < 0)
-					this.books.push(book);
-			}
-		}
-		if (this.books.length == 0) {
-			this.importSuccess();
-		} else {
-			for (var j = 0; j < this.books.length; j++) {
-				this.addBook(this.books[j]);
-			}
-		}
-	},
 	loadBooks: function() {
 		this.$.kindleImport.loadBookData(enyo.bind(this, 
 			function(books) {
 				var msg = "";
+				var d = new Date();
 				for (var i = 0; i < books.length; i++) {
-					//msg += books[i];
-					if(books[i].indexOf("86") > -1)
-						msg = books[i];
+					var asin = "Y" + d.getFullYear() + "M" + d.getMonth() + "D" + d.getDate() +
+						"H" + d.getHours() + "S" + d.getSeconds() + "N" + i;
+					if (this.bookAsinsInDB.indexOf('"' + asin + '"') > -1)
+						continue;
+					var parts = books[i].split(" - ");
+					var title = parts[0];
+					var author = parts[1].substring(0, parts[1].indexOf(".mobi"));
+					var b = new Book();
+					b.asin = asin;
+					b.title = title;
+					b.author = author;
+					b.file = books[i];
+					this.importCount++;
+					this.books.push(b);
+					// for now until the renderBookList function is implemented
+					this.importBooks();
 				}
-				new File("file:///media/internal/.kindle-import/" + msg, this.createNewMobi.bind(this), this);
+				
 			}
 		));
+	},
+	renderBookList: function() {
+		// render a list of books in the import directory but not in the database
+		// display the filename, asin, title, author, description?, locationsTotal, locationsCompleted
+		// and allow all but the filename to be edited
+	},
+	importBooks: function() {
+		// Loop through the books and add them
+		for (var i = 0; i < this.books.length; i++)
+			addBook(this.books[i]);
+		
+		this.importSuccess();
 	},
 	booksInDB: function(asin) {
 		var fquery = { "select": ["asin"],
@@ -116,8 +105,8 @@ enyo.kind ({
 			"coverImagePath": "",
 			"guid": ":A6CD34B2",
 			"isDeleted": "0",
-			"locationsTotal": "15000",
-			"locationsCompleted": "1",
+			"locationsTotal": book.locationsTotal,
+			"locationsCompleted": book.locationsCompleted,
 			"nearestLocation": "0",
 			"numMarkups": "0",
 			"title": book.title,
@@ -131,7 +120,7 @@ enyo.kind ({
 		var objs = [param];
 		this.$.addBooksEntry.call({objects: objs});
 	}, 
-	importSuccess: function(inSender, inResponse) {
+	importSuccess: function() {
 		this.openDialog(this.books.length + " were successfully imported", true);
 		
 		setTimeout(enyo.bind(this, function() {
@@ -145,119 +134,6 @@ enyo.kind ({
 	},
 	dbFailure: function(inSender, inResponse) {
 		this.openDialog("Operation failed", true);
-	},
-	createNewDoc: function(file, doc) {
-		if (!file.ready) {
-			this.openDialog("Could not load file: " + file.getBasename());
-			return;
-		}
-		new DocReader(file, this.createNewPPrs.bind(this));
-	},
-	createNewPPrs: function(file, doc) {
-		if (doc == null) {
-			new PPrsReader(file, this.createNewMobi.bind(this), null);
-			return;
-		}
-		this.openDialog("It's a Doc");
-	},
-	createNewMobi: function(file, doc) {
-		//if (doc == null) {
-			new MobiReader(file, this.handleNewMobi.bind(this), null);
-			//return;
-		//}
-		//this.openDialog("It's a PPrs");
-	},
-	handleNewMobi: function(file, doc) {
-		//A mobi file should have a full name
-		var name = doc.header.mobi.fullName;
-		if (name == null || name.length <= 0) {
-			name = doc.pdb.name;	
-		} else {
-			//We must replace HTML escape sequences
-			name = HTMLParser.translateEscapes(name);
-		}
-		
-		/*var msg = "";
-		for (var i = 0; i < doc.attribute.length; i++)
-			msg += doc.attribute[i];
-			
-		this.openDialog(msg);*/
-		
-		
-		this.openDialog(doc.header.mobi.fullName + // Aspho Fields
-						" " + doc.header.attribute.recordCount + // 178
-						" " + doc.header.attribute.recordSize + // 4096
-						" " + doc.pdb.name + // Aspho_Fields
-						" " + doc.header.exth.coverImage + //<center><img label="00002"/></center>
-						" " + doc.pdb.appInfoId + //0
-						" " + doc.pdb.numRecords + //187
-						" " + doc.pdb.sortInfoID // 0
-						);
-		
-		
-		/*
-			in doc.header.attribute
-			******************
-		 	compression: 0,
-	        textLength: 0,
-	        *recordCount: 0,
-	        *recordSize: 0,
-	        *currReadingPos: 0
-		    *this.name="";
-			this.version = 0;
-			
-			in doc.header.mobi
-			**************************
-			headerLen: 0,
-			firstNonBookRecord: 0,
-			*fullName: null,
-			encryption: 0,
-			exthFlags: 0,
-			exthOffset: 0,
-			imageOffset: 0,
-			drmOffset: 0,
-			drmCount: 0,
-			drmSize: 0,
-			drmFlags: 0,
-			drmEffectiveDate: undefined,
-			drmExpireDate: undefined,
-			extraDataFlags: 0,
-			isLZ77Compressed: false,
-			isHuffCdicCompressed: false,
-			isHuffCdicSupported: false,
-			isDrmEncrypted: false,
-			isDrmVersionSupported: false
-			
-			in doc.header.exth
-			********************
-			id: "",
-			headerLen: 0,			
-			recordCount: 0,
-			*coverImage: ""
-			
-			in doc
-			*******************
-			this.attribute = new Object();
-			this.attribute.ro = false;
-			this.attribute.dirty = false;
-			this.attribute.backup = false;
-			this.attribute.overwrite = false;
-			this.attribute.reset = false;
-			this.attribute.noBeam = false;
-			
-			this.creationDate = 0;
-			this.modificationDate = 0;
-			this.backupDate = 0;
-			this.modificationNum = 0;
-			*this.appInfoId = 0;
-			*this.sortInfoID = 0;
-			this.type = "NONE";
-			this.creator = "DEFD";
-			this.uniqueIDseed = 0;
-			this.nextRecordListID = 0;
-			
-			*this.numRecords = 0;
-		*/
 	},
 	showAbout: function(inSender, inResponse) {
 		this.openDialog("Kindle Import - Use at your own risk!");
@@ -279,13 +155,9 @@ enyo.kind({
 	title: "",
 	author: "",
 	file: "",
-	setup: function(bookString) {
-		var bookArray = bookString.split("|");
-		this.asin = bookArray[0];
-		this.title = bookArray[1];
-		this.author = bookArray[2];
-		this.file = bookArray[3];
-		
-		return this;
-	}
+	locationsTotal: 15000,
+	locationsCompleted: 1,
+	description: "",
+	doImport: true,
+	doDelete: false
 });
