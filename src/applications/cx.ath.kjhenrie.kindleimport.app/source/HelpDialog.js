de1d1fc8976b987f13000eb8058a1c0d5de31941
Kirt Henrie <kjhenrie@yahoo.com>
2011-09-19 08:25:58 -0600

    Updated README and Help Dialog to reflect the latest version

--- src/applications/cx.ath.kjhenrie.kindleimport.app/source/HelpDialog.js
+++ src/applications/cx.ath.kjhenrie.kindleimport.app/source/HelpDialog.js
@@ -4,28 +4,14 @@ enyo.kind({
 	components: [
 		{style: "font-size: 14px; padding: 20px", components: [
 			{content: "<b>Kindle Import Help</b></br></br>", style: "font-size: 18px"},
-			{content:"<br/>1. You must grant the cx.ath.kjhenrie.kindleimport application 'create' permission for com.palm.kindle.books:1." +
-					" One method for doing this is to create a file named <i>com.palm.kindle</i> with the contents below in the /etc/palm/db/permissions folder on the TouchPad. You will need to restart the TouchPad after creating this file." +
-					"<br/><br/>[" +
-					"<br/>&nbsp;{" +
-					"<br/>&nbsp;&nbsp;\"type\": \"db.kind\"," +
-					"<br/>&nbsp;&nbsp;\"object\": \"com.palm.kindle.books:1\"," +
-					"<br/>&nbsp;&nbsp;\"caller\": \"cx.ath.kjhenrie.kindleimport.*\"," +
-					"<br/>&nbsp;&nbsp;\"operations\": {" +
-					"<br/>&nbsp;&nbsp;&nbsp;\"create\": \"allow\"," +
-					"<br/>&nbsp;&nbsp;&nbsp;\"read\": \"allow\"," +
-					"<br/>&nbsp;&nbsp;&nbsp;\"update\": \"allow\"," +
-					"<br/>&nbsp;&nbsp;&nbsp;\"delete\": \"allow\"" +
-					"<br/>&nbsp;&nbsp;}" +
-					"<br/>&nbsp;}" +
-					"<br/>]"},
-			{content: "2. Copy the books you wish to import into /media/internal/.palmkindle on your TouchPad or use Calibre and send the books to the device"},
-			{content: "&nbsp;&nbsp;The file name(s) should be in the format <i>Title - Author - ASIN.mobi</i>.  <b>ASIN is optional.</b>"},
-			{content: "3. (Coming Soon!) Modify the book metadata if you do not like the defaults and/or deselect books you do not wish to import"},
-			{content: "4. Click on Load Books button to load the books from the .palmkindle folder"},
-			{content: "5. Click on Import Books button to import the books into the database<br/><br/>"},
+			{content: "1. Copy the books you wish to import into /media/internal/.palmkindle on your TouchPad or " +
+				"use Calibre and send the books to the device, or use the script written by m0ngr31 (see http://forums.precentral.net/hp-touchpad-homebrew/297377-real-kindle-sideloading-solution.html for details)."},
+			{content: "&nbsp;&nbsp;The file name(s) should be in the format <i>Title - Author.mobi, Title - Author - ASIN.mobi, or <i>Title - Author - ASIN - Locations.mobi</i></i>"},
+			{content: "2. (Coming Soon!) Modify the book metadata if you do not like the defaults and/or deselect books you do not wish to import"},
+			{content: "3. Click on Load Books button to load the books from the .palmkindle folder"},
+			{content: "4. Click on Import Books button to import the books into the database<br/><br/>"},
 			{content: "You can use the Delete button to remove all books imported by this utility from Kindle.  This does not delete the book files."},
-			{content: "<br/>Roadmap: Add support for cover images. Add fields to edit the book metadata"},
+			{content: "<br/>Roadmap: Add support for cover images when sent from Calibre (m0ngr31 script supports this). Add fields to edit the book metadata"},
 			{content: "<br/>Click anywhere outside of this dialog to close it"}
 		]}
 	]
