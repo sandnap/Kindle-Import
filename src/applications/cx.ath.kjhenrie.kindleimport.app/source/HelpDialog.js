7596b3d31bc22ad528d66146eb6cbf0bd5cdbed6
Kirt Henrie <kjhenrie@yahoo.com>
2011-09-20 20:11:11 -0600

    Added launch icon and updated help documentation

--- src/applications/cx.ath.kjhenrie.kindleimport.app/source/HelpDialog.js
+++ src/applications/cx.ath.kjhenrie.kindleimport.app/source/HelpDialog.js
@@ -4,12 +4,12 @@ enyo.kind({
 	components: [
 		{style: "font-size: 14px; padding: 20px", components: [
 			{content: "<b>Kindle Import Help</b></br></br>", style: "font-size: 18px"},
-			{content: "1. Copy the books you wish to import into /media/internal/.palmkindle on your TouchPad or " +
-				"use Calibre and send the books to the device, or use the script written by m0ngr31 (see http://forums.precentral.net/hp-touchpad-homebrew/297377-real-kindle-sideloading-solution.html for details)."},
-			{content: "&nbsp;&nbsp;The file name(s) should be in the format <i>Title - Author.mobi, Title - Author - ASIN.mobi, or <i>Title - Author - ASIN - Locations.mobi</i></i>"},
-			{content: "2. (Coming Soon!) Modify the book metadata if you do not like the defaults and/or deselect books you do not wish to import"},
-			{content: "3. Click on Load Books button to load the books from the .palmkindle folder"},
-			{content: "4. Click on Import Books button to import the books into the database<br/><br/>"},
+			{content: "<br/>1. Copy the books you wish to import into /media/internal/.palmkindle on your TouchPad or " +
+				"use Calibre and send the books to the device, or use the script written by m0ngr31 which can be downloaded " +
+				"from http://www.joeipson.com/kindleSide.zip."},
+			{content: "<br/>The file name(s) should be in one of the formats:<br/><i>Title - Author.mobi<br/>Title - Author - ASIN.mobi<br/>Title - Author - ASIN - Locations.mobi</i>"},
+			{content: "<br/>2. Click on Load Books button to load the books from the .palmkindle folder"},
+			{content: "3. Click on Import Books button to import the books into the database<br/><br/>"},
 			{content: "You can use the Delete button to remove all books imported by this utility from Kindle.  This does not delete the book files."},
 			{content: "<br/>Roadmap: Add support for cover images when sent from Calibre (m0ngr31 script supports this). Add fields to edit the book metadata"},
 			{content: "<br/>Click anywhere outside of this dialog to close it"}
